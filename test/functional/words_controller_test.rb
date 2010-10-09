58e0cd9ad030229575240ec4b45613bbd2dfd2af
Jimmy Dee <jdee@csinitiative.com>
2010-10-09 12:32:37 -0500

    more functional controller tests

--- test/functional/words_controller_test.rb
+++ test/functional/words_controller_test.rb
@@ -1,9 +1,21 @@
 require 'test_helper'
 
 class WordsControllerTest < ActionController::TestCase
-  test "should get index" do
+  should "get index" do
     get :index
     assert_response :success
+  end
+
+  should "get show view" do
+    get :show, 'name' => 'slang'
+    assert_response :success
+    assert_not_nil assigns(:words)
+  end
+
+  should "get by_letter view" do
+    get :by_letter, 'letter' => 'E'
+    assert_response :success
+    assert_not_nil assigns(:words)
   end
 
 end
