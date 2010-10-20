e2985036b5a18301573a20555e033ff5bc71c9cc
Jimmy Dee <jimmy@dee.name>
2010-10-20 11:40:13 -0500

    better deployment location

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,10 +2,9 @@ set :user, 'dubsar'
 set :domain, 'dubsar-dictionary.com'
 set :application, "dubsar"
 set :repository,  "git@github.com:jdee/dubsar.git"
-set :deploy_to, "/home/#{user}/#{application}"
-
+set :use_sudo, false
+set :deploy_to, "/var/lib/#{application}"
 set :scm, :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
 role :web, domain
 role :app, domain
@@ -13,14 +12,14 @@ role :db,  domain, :primary => true # This is where Rails migrations will run
 
 default_run_options[:pty] = true
 
-# If you are using Passenger mod_rails uncomment this:
-# if you're still using the script/reapear helper you will need
-# these http://github.com/rails/irs_process_scripts
-
 namespace :deploy do
-  task :start do ; end
+  task :start, :roles => :app, :except => { :no_release => true } do
+    run "touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+
   task :stop do ; end
+
   task :restart, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+    run "touch #{File.join(current_path,'tmp','restart.txt')}"
   end
 end
