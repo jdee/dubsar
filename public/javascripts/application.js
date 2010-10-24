9ca798bc1a3ea9b4cf37e43db79d7156675ba1ae
Jimmy Dee <jimmy@dee.name>
2010-10-23 19:09:17 -0500

    corrected some positioning for slight style change due to addition of .ui-widget class

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -13,7 +13,7 @@
 
   function post_info(){
     /* just cheat off the stylesheet and don't try to compute the offset */
-    $main_div.stop().animate({ top: '22.8ex' }, 'fast');
+    $main_div.stop().animate({ top: '24.7ex' }, 'fast');
     $error_div.stop().replaceWith('<div id="error" class="ui-state-highlight ui-corner-all"><span class="ui-icon ui-icon-info"></span>working...</div>');
   }
 
