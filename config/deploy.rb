95ec66472670d608ba0535121c987ae456528249
Jimmy Dee <jimmy@dee.name>
2010-10-26 20:12:00 -0500

    with rvm/capistrano integration, get rid of the kluge

--- config/deploy.rb
+++ config/deploy.rb
@@ -42,7 +42,7 @@ namespace :deploy do
   desc 'Create asset packages for production'
   task :package_assets, :roles => :app do
     run <<-EOF
-      cd #{deploy_to}/current && /usr/local/rvm/gems/ree-1.8.7-2010.02/bin/bundle exec rake RAILS_ENV=production asset:packager:build_all
+      cd #{deploy_to}/current && bundle exec rake RAILS_ENV=production asset:packager:build_all
     EOF
   end
 
