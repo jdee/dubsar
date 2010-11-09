85c2c6017debfdb86c9297890c857666b43e477e
Jimmy Dee <jimmy@dee.name>
2010-11-09 08:02:16 -0600

    better numeric display

--- app/helpers/words_helper.rb
+++ app/helpers/words_helper.rb
@@ -2,4 +2,12 @@ module WordsHelper
   def frame_spanner(frame)
     frame.gsub('PP', '<span title="prepositional phrase">PP</span>')
   end
+
+  def model_count(model)
+    count = eval(model.capitalize).count.to_s
+
+    # there must be a more standard method, but for now I'm kluging this
+    md = /^(\d+)(\d{3})$/.match count
+    md ? md[1] + ',' + md[2] : count
+  end
 end
