b22aaf0addae3aa2bac71a21908c9e79c934a316
Jimmy Dee <jimmy@dee.name>
2010-11-09 17:20:49 -0600

    shift this tooltip problem a little

--- public/javascripts/application-ui.js
+++ public/javascripts/application-ui.js
@@ -315,6 +315,7 @@
         $tt.html($('div.template', this).html());
         $('a.close-icon-span', $tt).css({opacity:0});
         $tt.show();
+        $('> *', $tt).width($tt.innerWidth());
         $tt_gloss = $(this).addClass('ui-state-highlight').css({'border-style':'none'});
       },
       function(){
