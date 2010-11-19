7bf1916aef3b5644e9fc00172e8b2135550cd1c8
Jimmy Dee <jimmy@dee.name>
2010-11-19 15:23:12 -0600

    static asset domain

--- config/environments/backup.rb
+++ config/environments/backup.rb
@@ -63,4 +63,6 @@ Dubsar::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  ActionController::Base.asset_host = 'static.dubsar-dictionary.com'
 end
