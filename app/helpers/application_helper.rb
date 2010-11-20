94e31f4dda328789273bd2949c190499a557b0a3
Jimmy Dee <jimmy@dee.name>
2010-11-19 21:49:33 -0600

    get tour pix from a static-asset server

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,12 +16,24 @@
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 module ApplicationHelper
   def thumbnail_link_tag
+    "<link rel='thumbnail' href='#{asset_host}/images/dubsar.png' type='image/png'/>"
+  end
+
+  def tour_link_tag(name, description)
+    tag = <<TAG
+<a href='#{asset_host}/images/#{name}.png' title='#{name}' id='tour-#{name}'>
+  <h3>#{CGI.escapeHTML description}</h3>
+  <img src='#{asset_host}/images/#{name}-thumbnail.png' alt='#{name}'/>
+</a>
+TAG
+  end
+
+  def asset_host
     host = ActionController::Base.asset_host
     if host
       host = 'http://' + host
     else
       host = ''
     end
-    "<link rel='thumbnail' href='#{host}/images/dubsar.png' type='image/png'/>"
   end
 end
