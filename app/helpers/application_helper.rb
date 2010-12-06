6acd4490e7882960463c450b1e340cad6054c1f3
Jimmy Dee <jimmy@dee.name>
2010-12-05 20:10:07 -0600

    no borders

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -71,7 +71,7 @@ EOF
   def delicious_link
     s = <<EOF
 <a href="http://www.delicious.com/save" onclick="window.open('http://www.delicious.com/save?v=5&noui&jump=close&url='+encodeURIComponent(location.href)+'&title='+encodeURIComponent(document.title), 'delicious','toolbar=no,width=550,height=550'); return false;">
-  <img src="http://l.yimg.com/hr/img/delicious.small.gif" height="10" width="10" alt="Delicious" />
+  <img src="http://l.yimg.com/hr/img/delicious.small.gif" height="10" width="10" alt="Delicious" style="border-style: none;" />
 </a>
 EOF
   end
