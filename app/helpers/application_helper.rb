3466ae40e3a8c774e384f6409032d4161bd185f8
Jimmy Dee <jimmy@dee.name>
2010-12-12 18:34:03 -0600

    simplification

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -51,10 +51,12 @@ EOF
 
   def delicious_link(url='http://dubsar-dictionary.com', title='Dubsar')
     s = <<EOF
-<img src="http://l.yimg.com/hr/img/delicious.small.gif" height="10" width="10" alt="Delicious" style="border-style: none;" />
-<a href="http://www.delicious.com/save" onclick="window.open('http://www.delicious.com/save?v=5&noui&jump=close&url='+encodeURIComponent('#{url}')+'&title='+encodeURIComponent('#{title}'), 'delicious','toolbar=no,width=550,height=550'); return false;" id="delicious-text">
-  Bookmark this on Delicious
-</a>
+<span id="delicious-link">
+  <img src="http://l.yimg.com/hr/img/delicious.small.gif" height="10" width="10" alt="Delicious" style="border-style: none;" />
+  <a href="http://www.delicious.com/save" onclick="window.open('http://www.delicious.com/save?v=5&noui&jump=close&url='+encodeURIComponent('#{url}')+'&title='+encodeURIComponent('#{title}'), 'delicious','toolbar=no,width=550,height=550'); return false;" id="delicious-text">
+    Bookmark this on Delicious
+  </a>
+</span>
 EOF
   end
 
