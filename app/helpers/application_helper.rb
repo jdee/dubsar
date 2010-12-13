94d2399f75d7849401d50de55602b14c81c54a6d
Jimmy Dee <jimmy@dee.name>
2010-12-12 19:10:43 -0600

    get rid of them all

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -49,17 +49,6 @@ EOF
     end
   end
 
-  def delicious_link(url='http://dubsar-dictionary.com', title='Dubsar')
-    s = <<EOF
-<span id="delicious-link">
-  <img src="http://l.yimg.com/hr/img/delicious.small.gif" height="10" width="10" alt="Delicious" style="border-style: none;" />
-  <a href="http://www.delicious.com/save" onclick="window.open('http://www.delicious.com/save?v=5&noui&jump=close&url='+encodeURIComponent('#{url}')+'&title='+encodeURIComponent('#{title}'), 'delicious','toolbar=no,width=550,height=550'); return false;" id="delicious-text">
-    Bookmark this on Delicious
-  </a>
-</span>
-EOF
-  end
-
   def theme_color
     case @theme
     when 'light'
