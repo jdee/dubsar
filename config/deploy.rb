b9450aff831fa53e5bf33e4bd1f64e6225cb14c3
Jimmy Dee <jimmy@dee.name>
2011-02-26 18:43:51 -0600

    attempt an upgrade to ree-1.8.7-2011.01

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,13 +15,11 @@
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
 require 'bundler/capistrano'
+require 'rvm/capistrano'
 
-# From rvm.beginrescueend.com
-$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
-require "rvm/capistrano"                  # Load RVM's capistrano plugin.
-
-set :rvm_ruby_string, 'ree'        # Or whatever env you want it to run in.
+set :rvm_ruby_string, 'ree-1.8.7-2011.01'
 
 set :user, 'dubsar'
 set :domain, 'dubsar-dictionary.com'
