bc700f9e57650eae77891f2d2116a99057afb18b
Jimmy Dee <jimmy@dee.name>
2011-06-22 21:29:01 -0500

    more view specs

--- spec/views/words/show.html.haml_spec.rb
+++ spec/views/words/show.html.haml_spec.rb
@@ -34,8 +34,18 @@ describe '/words/show.html.haml' do
   it 'should have a search form' do
     render :layout => 'layouts/application', :template => 'words/show.html.haml'
     rendered.should have_selector('form', :method => 'get', :action => root_path) do |form|
-      form.should have_selector('input', :type => 'text')
-      form.should have_selector('button', :type => 'submit')
+      form.should have_selector('input', :type => 'text', :name => 'term', :id => 'word-input')
+      form.should have_selector('button', :type => 'submit', :id => 'word-submit')
+      form.should have_selector('input', :type => 'checkbox', :name => 'match', :id => 'word-case', :value => 'case')
+    end
+  end
+
+  it 'should have alphabet browsing links' do
+    render :layout => 'layouts/application', :template => 'words/show.html.haml'
+    ('A'..'Z').each do |letter|
+      rendered.should have_selector('a', :href => url_for(:controller => :words, :action => :show, :term => "^[#{letter}#{letter.downcase}]", :match => 'regexp', :title => "#{letter}")) do |a|
+        a.should contain(letter)
+      end
     end
   end
 
