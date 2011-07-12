3faf6fc818e2438d6ae9fa3303f403087a83fab0
Jimmy Dee <jimmy@dee.name>
2011-07-12 11:13:06 -0500

    added a SynsetsController

---
+++ spec/controllers/synsets_controller_spec.rb
@@ -0,0 +1,33 @@
+#  Dubsar Dictionary Project
+#  Copyright (C) 2010-11 Jimmy Dee
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+require 'spec_helper'
+
+describe SynsetsController do
+  context "handling basic routing and requests" do
+    before :each do
+      request.env['HTTP_REFERER'] = '/'
+    end
+
+    it "gets the :show view" do
+      food, grub = create_synonyms!
+      get :show, 'id' => food.synsets.first.id
+      response.should be_success
+      assigns(:synset).should_not be_blank
+    end
+  end
+end
