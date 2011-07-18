9572089a1aff327329328f59345a575710ec8762
Jimmy Dee <jimmy@dee.name>
2011-07-18 16:44:41 -0500

    pebcak

--- public/javascripts/application-ui.js
+++ public/javascripts/application-ui.js
@@ -289,8 +289,7 @@
       $('div#opensearch-buttonset').buttonset();
       $('button#opensearch-add').button({icons:{primary:'ui-icon-plus'}})
         .live('click', function() {
-        window.external.AddSearchProvider('http://dubsar-dictionary.com/opensearch.xml');
-        /* window.external.AddSearchProvider(window.location.protocol + "//" + window.location.host + $('a#opensearch-link').attr('href')); */
+        window.external.AddSearchProvider(window.location.protocol + "//" + window.location.host + $('a#opensearch-link').attr('href'));
         $('div#opensearch-dialog').dialog('close');
         return false;
       });
