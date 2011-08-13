e0f1efa5178448b3f987c09fb9b26562be980223
Jimmy Dee <jimmy@dee.name>
2011-08-13 15:39:21 -0500

    sort word senses in descending order of frequency count

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -230,7 +230,7 @@ class WordsController < ApplicationController
   end
 
   def json_show_response
-    senses = @word.senses.map do |s|
+    senses = @word.senses.all(:order => 'freq_cnt DESC').map do |s|
       [ s.id, s.synset.senses_except(s.word).map{|_s| [ _s.id, _s.word.name ]}, s.synset.gloss, s.synset.lexname, s.marker, s.freq_cnt ]
     end
     [ @word.id, @word.name, @word.pos, @word.other_forms, senses, @word.freq_cnt ]
