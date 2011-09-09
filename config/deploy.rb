2c0cee73f300ef3a5746a64a8679237ae67b3080
Jimmy Dee <jimmy@dee.name>
2011-09-08 21:05:48 -0500

    take three

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,6 +32,7 @@ set :deploy_to, "/var/lib/#{application}"
 set :scm, :git
 set :rails_env, 'production'
 set(:shared_database_path) {"#{shared_path}/databases"}
+set(:shared_config_path) {"#{shared_path}/config"}
 
 role :web, domain
 role :app, domain
