c690f4a297a9ba20313afa4fd927d27214419017
Jimmy Dee <jgvdthree@gmail.com>
2013-08-08 10:34:07 -0700

    Notification of coming outage

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -190,7 +190,7 @@ class WordsController < ApplicationController
 
     # search and index use the same URL
     unless @term
-      # flash.now[:notice] = 'Happy Halloween. In honor of the occasion, the dark theme is free all day!'
+      flash.now[:notice] = 'The Dubsar server will be unavailable for a while over the weekend for maintenance. See News below for more information.'
       render :action => :index
       return
     end
