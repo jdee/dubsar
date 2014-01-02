76814bed81af2cb24f603917ad7b5f21dbac1616
Jimmy Dee <jgvdthree@gmail.com>
2014-01-01 18:53:27 -0800

    And more, mostly source code copyrights

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,5 +1,5 @@
 #  Dubsar Dictionary Project
-#  Copyright (C) 2010-13 Jimmy Dee
+#  Copyright (C) 2010-14 Jimmy Dee
 #
 #  This program is free software; you can redistribute it and/or
 #  modify it under the terms of the GNU General Public License
