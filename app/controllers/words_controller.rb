3fd983c0e2239b2c0199a4bab31017baf8073147
Jimmy Dee <jgvdthree@gmail.com>
2015-01-18 19:25:21 -0800

    Convert m_support to new layout

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -48,10 +48,6 @@ class WordsController < ApplicationController
     render :layout => 'mobile'
   end
 
-  def m_support
-    render :layout => 'mobile'
-  end
-
   def m_license
     render :layout => false
   end
